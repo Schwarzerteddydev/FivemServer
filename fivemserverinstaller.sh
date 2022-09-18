6db9d544f52b48ca0710752720f69ce1822bd2db
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2022-09-18 18:55:31 +0200

    Update fivemserverinstaller.sh

--- fivemserverinstaller.sh
+++ fivemserverinstaller.sh
@@ -16,5 +16,7 @@ wget https://raw.githubusercontent.com/Schwarzerteddydev/FivemServer/main/start.
 chmod +x start.sh
 echo 'Install Screen'
 apt install screen
+echo 'cd /home/FiveM'
+cd /home/FiveM
 echo 'Erfolgreich installiert, Jetzt Must du in den Ordner cd /home/FiveM/ und die datei start.sh ausführen --> ./start.sh'
 echo 'Successfully installed, Now you must go to the folder cd /home/FiveM/ and execute the file start.sh -->./start.sh'
