0983b220c2db84d6c23751b74c2baf1deae242bf
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2022-09-18 18:27:35 +0200

    Update and rename fivemserverinstallerneu.sh to fivemserverinstallerneu1.sh

---
+++ fivemserverinstallerneu1.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+echo 'FiveM LINUX AutoInstaller, WITH ESX Included'
+apt update && apt upgrade
+apt install xf
+apt install tar
+mkdir -p /home/FiveM/server
+cd /home/FiveM/server
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5886-22ff65cf6e181d949570bca03b3d15e3e5ed55f1/fx.tar.xz
+tar xf fx.tar.xz
+echo 'Artifacts Installed'
+echo 'Installing Server-Data'
+rm -r fx.tar.xz
+
+echo 'Succesfully Installed, now add the sql of the folder [Sql] in /home/FiveM/server-data/[Sql] to your mysql server, that will create a database and then configure all the things of the admin in server.cfg'
