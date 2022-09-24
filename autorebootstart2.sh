a60483c5aa867de2135192a12e0f540a2524e864
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2022-09-24 13:44:55 +0200

    Update and rename autorebootstart1.sh to autorebootstart2.sh

--- autorebootstart2.sh
+++ autorebootstart2.sh
@@ -1,6 +1,6 @@
 echo 'FiveM LINUX REBOOT INSTALLER'
 apt update && apt upgrade
-touch /lib/systemd/system/fivem.service
+sudo nano /usr/bin/fivem_start.sh
 echo -e '
 [Unit]
 Description=FiveM server
@@ -14,10 +14,16 @@ ExecStart=/usr/bin/fivem_start.sh
 WantedBy=multi-user.target
 ' >> fivem.service
 
-   touch /usr/bin/fivem_start.sh
+   sudo nano /lib/systemd/system/fivem.service
    echo -e '
 #!/bin/bash
 screen -dmS txadmin bash  /home/FiveM/server/run.sh +set serverProfile default +set txAdminPort 40120
 
 ' >> fivem_start.sh
 echo 'FIVEM SERVER START WHIT SERVER REBOOT'
+
+sudo chmod +x /usr/bin/fivem_start.sh
+sudo systemctl daemon-reload
+sudo systemctl start fivem
+sudo systemctl enable fivem
+echo 'set permissions'
