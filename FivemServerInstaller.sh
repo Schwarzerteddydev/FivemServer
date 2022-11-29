dc2fd4caf28059e0a27ec1b7a2f82df8e5324e92
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2022-11-29 15:31:24 +0100

    Update FivemServerInstaller.sh

--- FivemServerInstaller.sh
+++ FivemServerInstaller.sh
@@ -5,7 +5,7 @@ apt install xf
 apt install tar
 mkdir -p /home/FiveM/server
 cd /home/FiveM/server
-wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/5949-fc523e6fb110df7b2d099c44c16df39dc5a0914c/fx.tar.xz
+wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/6086-3e7270d2320a8c8f4aba37eb964101dfc0007d21/fx.tar.xz
 tar xf fx.tar.xz
 echo 'Artifacts Installed'
 rm -r fx.tar.xz
