f39c4f1be12f39345413cd3a00d23a16607a7199
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2023-03-12 16:20:30 +0100

    Update FivemServerInstaller.sh

--- FivemServerInstaller.sh
+++ FivemServerInstaller.sh
@@ -19,7 +19,7 @@ echo 'cd /home/FiveM'
 cd /home/FiveM
 echo 'Erfolgreich installiert, Jetzt Must du in den Ordner cd /home/FiveM/ und die datei start.sh ausführen --> ./start.sh'
 echo 'Successfully installed, Now you must go to the folder cd /home/FiveM/ and execute the file start.sh -->./start.sh'
-echo 'Successfully installed, sERVER WURDE GESTARTET!!'
+echo 'Erfolgreich installiert, SERVER WURDE GESTARTET!!'
 echo 'Successfully installed, SERVER IS STARTING UP!!'
 echo 'uSE COMMAND screen -ls'
 echo 'BENUTZE DEN COMMAND screen -ls'
